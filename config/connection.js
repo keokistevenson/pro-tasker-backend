ea076ac8c3d06318d29ba4513890ce9ec988c9d1
keokistevenson <keokistevenson@gmail.com>
2026-05-25 22:27:45 -0400

    copied over connection.js from SBA.

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
- 
+// MongoDB connection setup
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI);
- 
-module.exports = mongoose.connection;
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("MongoDB connected.");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
