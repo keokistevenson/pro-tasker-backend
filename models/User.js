1635c8244a2a69b9fa10c1c6199567c1a70d2486
keokistevenson <keokistevenson@gmail.com>
2026-05-25 23:04:05 -0400

    Removed GitHub ID from the User model. Its not needed for this project.

--- models/User.js
+++ models/User.js
@@ -24,12 +24,6 @@ const userSchema = new Schema(
       type: String,
       minlength: 8,
     },
-
-    githubId: {
-      type: String,
-      unique: true,
-      sparse: true,
-    },
   },
   {
     timestamps: true,
