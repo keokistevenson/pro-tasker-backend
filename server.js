cc41cc1f49b643ed499430705b35b41e82b4a606
keokistevenson <keokistevenson@gmail.com>
2026-06-05 01:57:21 -0400

    Change message of the workign server to simply say port and not localhost.

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.get("/", (req, res) => {
 
 // PORT
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
